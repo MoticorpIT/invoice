af61b30aba666c17748aba4ac7eecaa9e606acdc
Yosi Benezra <yosi@moticorp.com>
2017-08-16 14:28:47 -0400

    Change name of app in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.master')
 @section('body')
-	<h1>InvYOS</h1>
+	<h1>Invoice App</h1>
 	<ul>
 		<li><a href="/customers">Customers</a></li>
 		<li><a href="/invoices">Invoices</a></li>
